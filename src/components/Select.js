5376e7b2c6c09d84eabcae6c53ab67f8f2add9b4
minnjeong <124552434+minnjeong@users.noreply.github.com>
2023-05-02 00:32:46 +0900

    Update Select.js
    select 드롭다운 수정 완료
    
    아쉬운 점 : 드롭다운 리스트들이 가운데에 정렬되면 좋겠습니다. css 부족!

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,52 +1,68 @@
 import React from "react";
 import { useState } from "react";
+import sync from "react-router-redux/lib/sync";
 import styled from "styled-components";
 
-function Select({ option }) {
-    const [contextMenuOpen, setContextMenuOpen] = useState(false);
 
-    const [selectedItem, setSelectedItem] = useState(option[0]);
+function Select({ option, option2 }) {
+
+    // 드롭다운 열고 닫기 
+    const [dropdownOpen, setDropdownOpen] = useState(false)
+    // 옵션 선택
+    const [selectedOption, setSelectedOption] = useState(option[0])
 
     const openContextMenu = () => {
-      if (contextMenuOpen) {
-        setContextMenuOpen(false);
-      } else {
-        setContextMenuOpen(true);
-      }
+        setDropdownOpen(!dropdownOpen);
     };
 
-    const selectMenu = (item) => {
-      setSelectedItem(item);
-      setContextMenuOpen(false);
+    const selectMenu = (e) => {
+        // console.log('selectMenu 실행됨')
+        setSelectedOption(e.target.textContent);
+        setDropdownOpen(false);
     };
 
     return (
-      <div>
-        <div>
-          <StSelectButton onClick={openContextMenu}>{selectedItem}</StSelectButton>
-          {contextMenuOpen && (
-            <div>
-              {option.map((item) => (
-                <div>
-                  <StSelectButton onClick={() => selectMenu(item)}>
-                    {item}
-                  </StSelectButton>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
-    );
-  }
-
+        <SelectArea>
+            <SelectButton onClick={openContextMenu}>
+                <span>{selectedOption}</span>
+                <span>▾</span>
+                <DropDown>
+                {dropdownOpen ? <div>
+                    {option.map((option) => {
+                        return (
+                            <Stdiv key={option} onClick={selectMenu} value={option}>{option}</Stdiv>
+                        )
+                    })}
+                </div> : null}
+                </DropDown>
+        </SelectButton >
+        </SelectArea >
+    )
+}
 
 export default Select;
 
-const StSelectButton = styled.button`
+const SelectButton = styled.button`
   width: 140px;
   height: 35px;
   background-color: #d9a7c7;
   border-radius: 12px;
   border: none;
 `;
+
+const Stdiv = styled.div`
+    width: 140px;
+    height: 35px;
+    border: none;
+    background-color: #d9a7c7;
+`
+
+const SelectArea = styled.div`
+display: flex;
+`
+
+const DropDown = styled.div`
+justify-content: center;
+display: flex;
+
+`
